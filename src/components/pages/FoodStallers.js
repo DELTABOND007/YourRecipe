91cbc608766d47d97d74ea73112953228ae0e4f1
HIRALAL UMESH KUMHAR <82142482+DELTABOND007@users.noreply.github.com>
2021-09-01 20:54:05 +0530

    Update FoodStallers.js

--- src/components/pages/FoodStallers.js
+++ src/components/pages/FoodStallers.js
@@ -1,29 +1,33 @@
 import React from 'react'
 import '../../App.css';
 import "./FoodStallers.css";
+import Footer from "../Footer";
 import { Carousel } from 'react-bootstrap';
 
 // import image1 from './../assets/images/1.jpg';
 export default function FoodStallers(){
     return (
+      <>
 
      <div className="food-stall">
      <Carousel fade={false} pause={false} controls={true}>
       <Carousel.Item>
          <img
           className="d-block"
-          //src={image1}
+          src='/images/image1.png'
           alt="First slide"
         /> 
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p></p>
+          
+          <p className="address">Address:2,Sardar Vallabbhai Patel Road,Himmat Nagar,Borivali West,Mumbai,Maharashtra 400013</p>
+          <p className="phone">Phone:098690 49410</p>
         </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item>
         <img
           className="d-block"
-          src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+          src="/images/image2.png"
           alt="second slide"
         />
         <Carousel.Caption>
@@ -31,10 +35,11 @@ export default function FoodStallers(){
           <p></p>
         </Carousel.Caption>
       </Carousel.Item>
+      
       <Carousel.Item >
         <img
           className="d-block"
-          src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg"
+          src="/images/image3.png"
           alt="Third slide"
         />
         <Carousel.Caption>
@@ -42,10 +47,97 @@ export default function FoodStallers(){
           <p></p>
         </Carousel.Caption>
       </Carousel.Item>
+
+      <Carousel.Item >
+        <img
+          className="d-block"
+          src="/images/image4.png"
+          alt="Fourth slide"
+        />
+        <Carousel.Caption>
+          <h3>Fourth slide label</h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item >
+        <img
+          className="d-block"
+          src="/images/image5.png"
+          alt="Fifth slide"
+        />
+        <Carousel.Caption>
+          <h3>Fifth slide label</h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item >
+        <img
+          className="d-block"
+          src="/images/image6.png"
+          alt="Sixth slide"
+        />
+        <Carousel.Caption>
+          <h3>Sixth slide label</h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item >
+        <img
+          className="d-block"
+          src="/images/image7.png"
+          alt="Seventh slide"
+        />
+        <Carousel.Caption>
+          <h3>Seventh slide label</h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item >
+        <img
+          className="d-block"
+          src="/images/image8.png"
+          alt="Eight slide"
+        />
+        <Carousel.Caption>
+          <h3>Eight slide label</h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item >
+        <img
+          className="d-block"
+          src="/images/image9.png"
+          alt="Ninth slide"
+        />
+        <Carousel.Caption>
+          <h3>Ninth slide label</h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item >
+        <img
+          className="d-block"
+          src="/images/image10.png"
+          alt="Tenth slide"
+        />
+        <Carousel.Caption>
+          <h3>Tenth slide label</h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
     </Carousel>
+
     
     
      </div>
+     <Footer/>
+     </>
  )
     
     
