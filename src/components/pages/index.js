8c31da84a93643ef15b119854af85bcee4f46c12
HIRALAL UMESH KUMHAR <hiralalumeshkumhar@gmail.com>
2021-09-22 18:06:07 +0530

    all set again

--- src/components/pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SearchBar from './SearchBar';
-
-import './index.css';
-ReactDOM.render(<div><SearchBar /></div>,document.getElementById('root'));
-
-
