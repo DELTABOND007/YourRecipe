952f76aeb8ab8c71a9f17dccb2cfd62602d32de4
Hira <hiralalumeshkumhar@gmail.com>
2021-09-22 13:16:35 +0530

    all set

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,24 @@
 import React from "react";
 
-
-
 const Header = (props) => {
     const { search, onInputChange, onSearchClick } = props;
     return (
     <div className="jumbotron">
-        
         <h1 className="display-1">
             <i class="icon ion-md-restaurant brand-icon"></i>YourRecipe<i class="icon ion-md-restaurant brand-icon"></i>
-
         </h1>
         <div className="input-group input w-50 mx-auto">
             <input type="text" class="form-control" placeholder="Search Your Recipe..."
-                value={search}
-                onChange={onInputChange}/>
-
+            value={search}
+            onChange={onInputChange}
+            />
             <div className="input-group-append ">
-                <button className="btn btn-dark"  onClick={onSearchClick}>
-                    Search
+            <button className="btn btn-dark"  onClick={onSearchClick}>
+            Search
                 </button>
             </div>
         </div>
-        
-        
-       
     </div>
-    
-    
     );
-
-
-    
-    
 }
 export default Header;
--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -4,7 +4,7 @@ const RecipeItem=(props)=>{
     const{name,image,ingredientLines}=props;
 return (
 <div class="card py-2 text-center">
-    <img src={image} className="img-fluid w-20 mx-auto rounded-circle"/>
+    <img src={image} className="img-fluid w-10 mx-auto rounded-circle"/>
     <div class="card-body">
         <h5>{name}</h5>
     </div>
