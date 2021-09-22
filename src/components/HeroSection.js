25344cbcea093782d728a65c8fb64df3fbab2da6
HIRALAL UMESH KUMHAR <hiralalumeshkumhar@gmail.com>
2021-09-22 19:04:46 +0530

    all set for now

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom"
+
 
 function HeroSection() {
   return (
